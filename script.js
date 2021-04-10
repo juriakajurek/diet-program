53a9e7801c5934607a9d66a088ed98cdecb0edf9
juriakajurek <juriakajurek@github.com>
2021-04-10 10:47:03 +0200

    Add hero section and footer, some fixes

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    setCost()
+    document.getElementById('patients').addEventListener('input', setCost)
 })
+
+function increasePatients() {
+    ++document.getElementById('patients').value
+    setCost()
+}
+function decreasePatients() {
+    --document.getElementById('patients').value
+    setCost()
+}
+
+function setCost() {
+    document.getElementById('cost').innerHTML =
+        document.getElementById('patients').value * 2 +
+        ',00 zł <span>z VAT</span>'
+}
